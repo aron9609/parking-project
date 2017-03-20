215599ef7999c095e45e14dc80f4261b9f6db144
aron9609 <aron9609@gmail.com>
2017-03-20 23:12:58 +0100

    updated template

--- parking-data/src/main/java/hu/aronszabo/ulyssys/parking/data/repository/CarRepository.java
+++ parking-data/src/main/java/hu/aronszabo/ulyssys/parking/data/repository/CarRepository.java
@@ -18,6 +18,14 @@ public class CarRepository {
         DATA.add(new CarDTO("KVE-229", "Opel", "Astra", "red"));
         DATA.add(new CarDTO("KVE-230", "Kia", "Rio", "black"));
         DATA.add(new CarDTO("KVE-231", "Ford", "Fiesta", "black"));
+        DATA.add(new CarDTO("KVE-232", "Suzuki", "SX4", "green"));
+        DATA.add(new CarDTO("KVE-233", "Skoda", "Fabia", "red"));
+        DATA.add(new CarDTO("KVE-234", "BMW", "320d", "black"));
+        DATA.add(new CarDTO("KVE-235", "Skoda", "Octavia", "black"));
+        DATA.add(new CarDTO("KVE-236", "Renault", "Clio", "white"));
+        DATA.add(new CarDTO("KVE-237", "Opel", "Corsa", "red"));
+        DATA.add(new CarDTO("KVE-238", "Volvo", "S40", "black"));
+        DATA.add(new CarDTO("KVE-239", "Ford", "Fiesta", "black"));
         log.debug(DATA.toString());
     }
 
